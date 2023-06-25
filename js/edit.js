24b5c161f207934f303ae8b582290d1b96188c6a
Paula Pacheco <100471162+ennxxx@users.noreply.github.com>
2023-06-25 12:28:58 +0800

    removed edit.js

--- js/edit.js
+++ js/edit.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded",function() {
 
 // .________________________.
-// ||			           ||
+// ||			                 ||
 // ||        Sign In       ||
 // ||______________________||
-// '			            '
+// '			                  '
 
 var signedIn = true;
 var userDropdown = document.getElementById("user-dropdown");
@@ -22,10 +22,10 @@ if (signedIn) {
 }
     
 // .________________________.
-// ||			           ||
+// ||			                 ||
 // ||    Change Profile    ||
 // ||______________________||
-// '			            '
+// '			                  ' 
 
 var changeButton = document.querySelector("#change-image-btn");
 changeButton.addEventListener("click", changeProfile);
@@ -41,10 +41,12 @@ function changeProfile() {
 	  var reader = new FileReader();
   
 	  reader.onload = function (e) {
-		var newProfilePic = document.getElementById("profile-pic");
-		newProfilePic.src = e.target.result;
-		var newProfile = document.getElementById("profile");
-		newProfile.src = e.target.result;
+      var newProfileMain = document.getElementById("profile-pic");
+      newProfileMain.src = e.target.result;
+      var newProfileNav = document.getElementById("profile");
+      newProfileNav.src = e.target.result;
+      var newProfilePost = document.getElementsByClassName("post-profile")[0];
+      newProfilePost.src = e.target.result;
 	  };
   
 	  reader.readAsDataURL(file);
@@ -52,46 +54,5 @@ function changeProfile() {
   
 	mediaInput.click();
   }
-
-// .________________________.
-// ||			           ||
-// ||     Change Info      ||
-// ||______________________||
-// '			            '
-
-    // Retrieve the name and bio input elements
-    var nameInput = document.getElementById('new-name');
-    var bioInput = document.getElementById('new-bio');
   
-    // Add event listener to the save button
-    document.getElementById('save').addEventListener('click', function () {
-      // Get the input values
-      var newName = nameInput.value;
-      var newBio = bioInput.value;
-  
-      // Check if the name and bio inputs are empty
-      if (newName.trim() === '' || newBio.trim() === '') {
-        alert('Name and bio cannot be empty!');
-        return;
-      }
-  
-      // Redirect to the profile page with URL parameters
-      window.location.href = 'uprofile.html?name=' + encodeURIComponent(newName) + '&bio=' + encodeURIComponent(newBio);
-    });
-  
-    // Get the URL parameters
-    var params = new URLSearchParams(window.location.search);
-    var newName = params.get('name');
-    var newBio = params.get('bio');
-  
-    // Update the name and bio elements on the profile page
-    if (newName && newBio) {
-      var nameElement = document.getElementById('name');
-      var bioElement = document.getElementById('bio');
-  
-      nameElement.innerHTML = newName;
-      bioElement.innerHTML = newBio;
-    }
-
-
 });
