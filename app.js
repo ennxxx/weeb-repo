5ab11b113076d90b6b0db55589d5f1472248c263
GeneCedricAlejo <97246520+GeneCedricAlejo@users.noreply.github.com>
2023-07-21 23:34:51 +0800

    Can now retrieve from db asynchronously.
    Also fixed the import function to not include items with duplicate post_id.

--- app.js
+++ app.js
@@ -1,182 +1,259 @@
+// Import express, express-handlebars, mongodb NodeJS modules.
 import express from 'express';
 import exphbs from 'express-handlebars';
-import * as helpers from './helpers.js';
-import 'dotenv/config';
 import { MongoClient } from 'mongodb';
-import { connectToMongo, getDb } from './db/conn.js';
+
+// Import environment variables from .env file. and fs module.
+import 'dotenv/config';
 import fs from 'fs';
 
+// Import functions from other js files.
+import { connectToMongo, getDb } from './db/conn.js';
+import * as helpers from './helpers.js';
 
+// Connects the app to the MongoDB database.
 connectToMongo((err) => {
+  // If there is an error, print it out and exit the process.
   if (err) {
-    console.log("May Error, Puta.");
+    console.log("Error connecting to MongoDB...");
     console.error(err);
     process.exit();
   }
+  // If there is no error, print out a success message.
   console.log("Connected to MongoDB!");
-  // Do stuff here daw
   const db = getDb();
 });
 
+// Creates an  instance of the express app.
 const app = express();
+// Retrieves the database name and MongoDB URI from the .env file.
 const dbName = process.env.DB_NAME;
 const mongoURI = process.env.MONGODB_URI;
+// Creates a variable that stores the database.
 const db = getDb();
 
+// This function imports the data from the JSON file into the database.
+// RIGHT NOW IT ONLY IMPORTS THE POSTS JSON FILE
 async function importData() {
   try {
-    const client = await MongoClient.connect(mongoURI);
-    const db = client.db(dbName);
-
+    // Declaration of the data to be manipulated.
     const collectionName = "PostsCollection";
     const dataToParse = fs.readFileSync('public/JSONs/Posts.json');
     const jsonData = JSON.parse(dataToParse);
     const collection = db.collection(collectionName);
 
-    // Insert the JSON data into the collection
-    const result = await collection.insertMany(jsonData);
-    console.log(`${result.insertedCount} documents inserted into ${collectionName}.`);
+    for (const post of jsonData) {
+      // Check if a document with the same post_id already exists in the collection
+      const existingID = await collection.findOne({ post_id: post.post_id });
 
-    client.close();
+      if (!existingID) {
+        // If the document with the same post_id doesn't exist, insert the JSON data into the collection
+        const result = await collection.insertOne(post);
+        console.log(`Post with id ${post.post_id} inserted.`);
+      } else {
+        console.log(`Post with id ${post.post_id} already exists. Skipping insertion.`);
+      }
+    }
+
   } catch (error) {
     console.error('Error importing data:', error);
   }
 }
 
-importData();
-const posts = db.collection("PostsCollection");
+// Make the importData function call await using an async IIFE
+(async () => {
+  try {
+    await connectToMongo();
+    console.log("Connected to MongoDB!");
 
-app.use("/static", express.static("public"));
+    await importData();
 
-app.engine('hbs', exphbs.engine({
-  extname: "hbs",
-  defaultLayout: "main",
-  helpers: {
-    capitalize: function (string) {
-      return string.toUpperCase();
-    },
-    substring: helpers.substring
-  }
-}));
+    // Start the Express server after importing the data
+    app.engine('hbs', exphbs.engine({
+      extname: "hbs",
+      defaultLayout: "main",
+      helpers: {
+        capitalize: function (string) {
+          return string.toUpperCase();
+        },
+        substring: helpers.substring
+      }
+    }));
 
-app.set("view engine", "hbs");
-app.set("views", "./views");
 
-app.get("/", (req, res) => {
-  res.render("index", {
-    title: 'Home',
-    posts: posts,
-    toppost: posts[5]
-  });
-});
+    app.use("/static", express.static("public"));
+    app.set("view engine", "hbs");
+    app.set("views", "./views");
 
-app.get("/view/:id", (req, res) => {
-  const id = req.params.id;
+    app.get("/", async (req, res) => {
+      try {
+        const collection = getDb().collection("PostsCollection");
+        const posts = await collection.find().toArray();
 
-  res.render("view", {
-    post: posts[id]
-  });
-});
-
-app.get("/main-profile", (req, res) => {
-
-  const filters = ['Overview', 'Posts', 'Comments', 'Upvoted', 'Downvoted', 'Saved'];
-
-  res.render("main-profile", {
-    posts: posts,
-    filters: filters
-  });
-});
-
-app.get("/profile/comments", (req, res) => {
-  const user = req.body;
-  console.log(user);
-  var postsByAuthor = posts.filter(function (post) {
-    return post.comments.some(function (comment) {
-      return comment.author == user;
+        res.render("index", {
+          title: 'Home',
+          posts: posts,
+          toppost: posts[5]
+        });
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
     });
-  });
 
-  res.render("main-profile", {
-    commentPosts: postsByAuthor,
-    filters: filters
-  });
-});
+    app.get("/view/:id", async (req, res) => {
+      const id = req.params.id;
+      try {
+        const collection = getDb().collection("PostsCollection");
+        const posts = await collection.find().toArray();
 
-/* app.get("/anime", (req, res) => {
-  res.render("anime");
-});*/
+        res.render("view", {
+          post: posts[id]
+        });
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
 
-// intercept all requests with the content-type, application/json
-app.use(express.json());
-// app.use(express.urlencoded({extended: true}));
+    app.get("/main-profile", async (req, res) => {
+      try {
+        const filters = ['Overview', 'Posts', 'Comments', 'Upvoted', 'Downvoted', 'Saved'];
+        const collection = getDb().collection("PostsCollection");
+        const posts = await collection.find().toArray();
 
-app.post("/post", (req, res) => {
-  console.log("POST Request to /post received.");
-  console.log(req.body);
-  const { title, author, content, image } = req.body;
-  if (title && author && content) {
-    const newPost = {
-      title: title,
-      author: author,
-      content: content,
-      image: image,
-      comments: [],
-      id: posts.length
-    };
-    posts.push(newPost);
-    res.status(200);
-    res.redirect("/");
+        res.render("main-profile", {
+          posts: posts,
+          filters: filters
+        });
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
+
+
+    app.get("/profile/comments", async (req, res) => {
+      try {
+        const collection = getDb().collection("PostsCollection");
+        const posts = await collection.find().toArray();
+        const user = req.body;
+        console.log(user);
+        var postsByAuthor = posts.filter(function (post) {
+          return post.comments.some(function (comment) {
+            return comment.author == user;
+          });
+        });
+
+        res.render("main-profile", {
+          commentPosts: postsByAuthor,
+          filters: filters
+        });
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
+
+    // intercept all requests with the content-type, application/json
+    app.use(express.json());
+
+
+    app.post("/post", async (req, res) => {
+      try {
+        const collection = getDb().collection("PostsCollection");
+        const posts = await collection.find().toArray();
+        console.log("POST Request to /post received.");
+        console.log(req.body);
+        const { title, author, content, image } = req.body;
+        if (title && author && content) {
+          const newPost = {
+            title: title,
+            author: author,
+            content: content,
+            image: image,
+            comments: [],
+            id: posts.length
+          };
+          posts.push(newPost);
+          res.status(200);
+          res.redirect("/");
+        }
+        else {
+          res.status(400);
+          res.redirect("/error");
+        }
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
+
+    app.post("/comment", async (req, res) => {
+      try {
+        const collection = getDb().collection("PostsCollection");
+        const posts = await collection.find().toArray();
+
+        console.log("POST Request to /post received.");
+        console.log(req.body);
+        const { author, content, profpic } = req.body;
+        const id = req.body.id;
+
+        if (author && content && id) {
+          const newComment = {
+            author: author,
+            content: content,
+            profpic: profpic,
+            comID: posts[id].comments.length,
+            reply: []
+          };
+          posts[id].comments.push(newComment);
+          res.status(200);
+          res.redirect("/view/:id");
+        }
+        else {
+          res.status(400);
+        }
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
+
+    app.post("/reply", async (req, res) => {
+      try {
+        const collection = getDb().collection("PostsCollection");
+        const posts = await collection.find().toArray();
+
+        console.log("POST Request to /post received.");
+        const { author, replyContent, profpic } = req.body;
+        const id = req.body.id;
+
+        const newReply = {
+          author: author,
+          content: replyContent,
+          profpic: profpic,
+          comID: posts[id].comments.length,
+          reply: []
+        };
+        if (newReply && id) {
+          posts[id].comments.push(newComment);
+          res.status(200);
+          res.redirect("/view/:id");
+        }
+        else {
+          res.status(400);
+        }
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
+
+    app.listen(3000, () => console.log("Server is running on port 3000"));
+  } catch (error) {
+    console.error("Error:", error);
+    process.exit(1);
   }
-  else {
-    res.status(400);
-    res.redirect("/error");
-  }
-});
+})();
 
-app.post("/comment", (req, res) => {
-  console.log("POST Request to /post received.");
-  console.log(req.body);
-  const { author, content, profpic } = req.body;
-  const id = req.body.id;
-
-  if (author && content && id) {
-    const newComment = {
-      author: author,
-      content: content,
-      profpic: profpic,
-      comID: posts[id].comments.length,
-      reply: []
-    };
-    posts[id].comments.push(newComment);
-    res.status(200);
-    res.redirect("/view/:id");
-  }
-  else {
-    res.status(400);
-  }
-});
-
-app.post("/reply", (req, res) => {
-  console.log("POST Request to /post received.");
-  const { author, replyContent, profpic } = req.body;
-  const id = req.body.id;
-
-  const newReply = {
-    author: author,
-    content: replyContent,
-    profpic: profpic,
-    comID: posts[id].comments.length,
-    reply: []
-  };
-  if (newReply && id) {
-    posts[id].comments.push(newComment);
-    res.status(200);
-    res.redirect("/view/:id");
-  }
-  else {
-    res.status(400);
-  }
-});
-
-app.listen(3000, () => console.log("Server is running on port 3000"));
--- db/conn.js
+++ db/conn.js
@@ -4,13 +4,16 @@ import { MongoClient } from 'mongodb';
 const mongoURI = process.env.MONGODB_URI;
 const client = new MongoClient(mongoURI);
 
-export function connectToMongo (callback) {
-    client.connect().then( (client) => {
-        return callback();
-    }).catch( err => {
-        callback(err);
+export function connectToMongo(callback) {
+    client.connect(mongoURI, (err, client) => {
+      if (err) {
+        callback(err); // Call the callback with the error if connection fails
+      } else {
+        const db = client.db(dbName);
+        callback(null, db); // Call the callback with the database instance if connection is successful
+      }
     });
-}
+  }
 
 export function getDb(dbName = process.env.DB_NAME) {
     return client.db(dbName);
