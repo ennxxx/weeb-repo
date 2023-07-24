e88374b95b3efb502271e1287ec197566b1a4601
GeneCedricAlejo <97246520+GeneCedricAlejo@users.noreply.github.com>
2023-07-24 21:26:06 +0800

    Finished edit profile

--- public/js/edit.js
+++ public/js/edit.js
@@ -5,91 +5,91 @@
 // '			            ' 
 
 async function editProfile() {
-    const newName = document.getElementById("new-name").value;
-    const newBio = document.getElementById("new-bio").value;
+  const newName = document.getElementById("new-name").value;
+  const newBio = document.getElementById("new-bio").value;
 
-    try {
-        const jString = JSON.stringify({ name: newName, bio: newBio });
-  
-        const response = await fetch(`/edit-profile`, {
-          method: 'PUT',
-          body: jString,
-          headers: {
-            "Content-Type": "application/json"
-          }
-        });
-  
-        if (response.status === 200) {
-          console.log("Post updated");
-          window.location.href = "/main-profile";
-          // Optionally, you can update the post content and image on the page here
-        } else {
-          console.error("Bad request");
-        }
-      } catch (error) {
-        console.error("Error during post update:", error);
+  try {
+    const requestBody = { name: newName, bio: newBio };
+
+    const response = await fetch(`/edit-profile`, {
+      method: 'PUT',
+      body: JSON.stringify(requestBody),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+
+    if (response.status === 200) {
+      console.log("Profile updated");
+      window.location.href = "/main-profile";
+      // Optionally, you can update the profile content on the page here
+    } else {
+      console.error("Bad request");
     }
-
+  } catch (error) {
+    console.error("Error during profile update:", error);
+  }
 }
+
 
 // Changing Profile Picture
 document.addEventListener("DOMContentLoaded", function () {
-    var changeButton = document.querySelector("#edit-change-image-btn");
-    changeButton.addEventListener("click", changeProfile);
+  var changeButton = document.querySelector("#edit-change-image-btn");
+  changeButton.addEventListener("click", changeProfile);
 
-    function changeProfile() {
-        var mediaInput = document.createElement("input");
-        mediaInput.type = "file";
-        mediaInput.accept = "image/*";
-        mediaInput.style.display = "none";
+  function changeProfile() {
+    var mediaInput = document.createElement("input");
+    mediaInput.type = "file";
+    mediaInput.accept = "image/*";
+    mediaInput.style.display = "none";
 
-        mediaInput.addEventListener("change", function (event) {
-            var file = event.target.files[0];
-            var reader = new FileReader();
+    mediaInput.addEventListener("change", function (event) {
+      var file = event.target.files[0];
+      var reader = new FileReader();
 
-            reader.onload = function (e) {
-                var newProfileMain = document.getElementById("profile-pic");
+      reader.onload = function (e) {
+        var newProfileMain = document.getElementById("profile-pic");
 
-                var canvas = document.createElement("canvas");
-                var context = canvas.getContext("2d");
+        var canvas = document.createElement("canvas");
+        var context = canvas.getContext("2d");
 
-                var image = new Image();
-                image.onload = function () {
-                    var imageWidth = image.width;
-                    var imageHeight = image.height;
+        var image = new Image();
+        image.onload = function () {
+          var imageWidth = image.width;
+          var imageHeight = image.height;
 
-                    var cropSize = 800; // Adjust this value to change the crop size
+          var cropSize = 800; // Adjust this value to change the crop size
 
-                    var cropX = Math.max(0, (imageWidth - cropSize) / 2);
-                    var cropY = Math.max(0, (imageHeight - cropSize) / 2);
-                    var cropWidth = Math.min(cropSize, imageWidth);
-                    var cropHeight = Math.min(cropSize, imageHeight);
+          var cropX = Math.max(0, (imageWidth - cropSize) / 2);
+          var cropY = Math.max(0, (imageHeight - cropSize) / 2);
+          var cropWidth = Math.min(cropSize, imageWidth);
+          var cropHeight = Math.min(cropSize, imageHeight);
 
-                    canvas.width = 300;
-                    canvas.height = 300;
+          canvas.width = 300;
+          canvas.height = 300;
 
-                    context.drawImage(
-                        image,
-                        cropX,
-                        cropY,
-                        cropWidth,
-                        cropHeight,
-                        0,
-                        0,
-                        300,
-                        300
-                    );
+          context.drawImage(
+            image,
+            cropX,
+            cropY,
+            cropWidth,
+            cropHeight,
+            0,
+            0,
+            300,
+            300
+          );
 
-                    newProfileMain.src = canvas.toDataURL();
-                };
+          newProfileMain.src = canvas.toDataURL();
+        };
 
-                image.src = e.target.result;
-            };
+        image.src = e.target.result;
+      };
 
-            reader.readAsDataURL(file);
-        });
+      reader.readAsDataURL(file);
+    });
 
-        mediaInput.click();
-    }
+    mediaInput.click();
+  }
 });
 
