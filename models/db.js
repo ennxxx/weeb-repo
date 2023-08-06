575ac5dc2d379cc6d889e5be18259e52bad6846c
Nigel <nigel_valero@dlsu.edu.ph>
2023-08-06 20:24:31 +0800

    updated db

--- models/db.js
+++ models/db.js
@@ -1,9 +1,10 @@
 import mongoose from 'mongoose';
 import 'dotenv/config';
 
+const url = 'mongodb+srv://nigelvalero:XloSADr3TnVr5S4C@ccapdev-weeblib.5vjgoqs.mongodb.net/ccapdev-weeblib?retryWrites=true&w=majority'
 export async function connectToDB() {
     try {
-        await mongoose.connect(process.env.PORT, {
+        await mongoose.connect(url, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
